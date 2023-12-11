1d89c29d6f9e79c5aa46855425519ceb03012c2a
dennis.bohn <dennis.bohn@bohnmedia.de>
2023-12-11 01:25:53 +0100

    Added support for multiple images

--- README.md
+++ README.md
@@ -1,12 +1,16 @@
 # PS1 TIM File Reader in JavaScript
 This JavaScript class generates a canvas from a TIM file. A TIM file is an image file format used by the PlayStation 1 game console.
 
-The class provides the following methods:
+The class provides the following methods and properties:
 
-| Method            | Description |
-| ----------------- | ----------- |
+| Method | Description |
+| --- | --- |
 | `readArrayBuffer(arrayBuffer)` | Reads the TIM file from an ArrayBuffer. |
-| `createCanvas()`    | Creates a canvas element for the DOM. |
+| `createCanvas()` | Creates a canvas element for the DOM. |
+
+| Property | Description |
+| --- | --- |
+| `length` | Number of images in the set. |
 
 ## Examples
 
@@ -25,7 +29,7 @@ fetch(url)
     timReader.readArrayBuffer(arrayBuffer);
 
     // Generate the canvas
-    const canvas = reader.createCanvas();
+    const canvas = timReader.createCanvas();
 
     // Append the canvas to the document body
     document.body.appendChild(canvas);
@@ -47,11 +51,32 @@ file.addEventListener("change", () => {
         timReader.readArrayBuffer(reader.result);
 
         // Generate the canvas
-        const canvas = reader.createCanvas();
+        const canvas = timReader.createCanvas();
 
         // Append the canvas to the document body
         document.body.appendChild(canvas);
     }
     reader.readAsArrayBuffer(file.files[0])
 });
+```
+
+## Working with multiple images
+
+A TIM file can contain a set of images. By using `createCanvas()` you can always extract the first image of the set. By using `createCanvas(index)` you can make a selection. By using the property `length` you get the number of images in the set.
+
+### Example
+
+```javascript
+// Parse the file
+timReader.readArrayBuffer(arrayBuffer);
+
+// Loop through the images
+for (let i = 0; i < timReader.length; i++) {
+
+    // Generate the canvas
+    const canvas = timReader.createCanvas(i);
+
+    // Append the canvas to the document body
+    document.body.appendChild(canvas);
+}
 ```
--- TimReader.js
+++ TimReader.js
@@ -1,132 +1,146 @@
 /**
  * TimReader 1.0
  * Creates canvas from a TIM-File
- * 
+ *
  * Copyright 2023, Dennis Bohn
  * bohn.media
  * https://www.bohnmedia.de/
- * 
+ *
  * Licensed under MIT
- * 
+ *
  * Released on: December 12, 2023
  */
 
 class TimReader {
+  constructor() {
+    this._images = [];
+  }
 
-    _read(bits = 32) {
-        const block = Math.floor(this._pointer / 32);
-        const offset = this._pointer % 32;
-        const bitmask = (1 << bits) - 1 || 0xFFFFFFFF;
-        const data = (this._uint32Array[block] >> offset) & bitmask;
-        this._pointer += bits;
-        return data;
+  createCanvas(imageIndex = 0) {
+    const data = this._images[imageIndex];
+
+    const canvas = document.createElement("canvas");
+    canvas.width = data.imageWidth;
+    canvas.height = data.imageHeight;
+
+    const ctx = canvas.getContext("2d");
+    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    for (let i = 0; i < data.imageColors.length; i++) {
+      ["r", "g", "b"].forEach((key, index) => {
+        imageData.data[index + i * 4] = data.imageColors[i][key];
+      });
+      imageData.data[3 + i * 4] = 255;
     }
 
-    _skip(bits) {
-        this._pointer += bits;
+    ctx.putImageData(imageData, 0, 0);
+
+    return canvas;
+  }
+
+  readArrayBuffer(arrayBuffer) {
+    this._uint32Array = new Uint32Array(arrayBuffer);
+    this._parse();
+  }
+
+  get length() {
+    return this._images.length;
+  }
+
+  _read(bits = 32) {
+    const block = Math.floor(this._pointer / 32);
+    const offset = this._pointer % 32;
+    const bitmask = (1 << bits) - 1 || 0xffffffff;
+    const data = (this._uint32Array[block] >> offset) & bitmask;
+    this._pointer += bits;
+    return data;
+  }
+
+  _skip(bits) {
+    this._pointer += bits;
+  }
+
+  _rewind() {
+    this._pointer = 0;
+  }
+
+  _parse() {
+    this._rewind();
+
+    this._images.splice(0, this._images.length);
+
+    while (this._read(8) === 16) {
+      const image = {};
+
+      image.version = this._read(8);
+
+      this._skip(16);
+
+      image.bpp = this._read(2);
+      image.clp = this._read(2);
+
+      this._skip(28);
+
+      if (image.clp) {
+        image.clutLength = this._read(32);
+        image.clutX = this._read(16);
+        image.clutY = this._read(16);
+        image.clutWidth = this._read(16);
+        image.clutHeight = this._read(16);
+        image.clutColors = this._readColors(image.clutWidth * image.clutHeight, 2);
+      }
+
+      image.imageLength = this._read(32);
+      image.imageX = this._read(16);
+      image.imageY = this._read(16);
+      image.imageWidth = this._read(16);
+      image.imageHeight = this._read(16);
+      switch (image.bpp) {
+        case 0:
+          image.imageWidth *= 4;
+          break;
+        case 1:
+          image.imageWidth *= 2;
+          break;
+        case 3:
+          image.imageWidth /= 1.5;
+          break;
+      }
+
+      image.imageColors = this._readColors(image.imageWidth * image.imageWidth, image.bpp, image.clutColors);
+
+      console.log(image);
+
+      this._images.push(image);
     }
+  }
 
-    _rewind() {
-        this._pointer = 0;
+  _readColors(length, bpp, clp) {
+    const colors = [];
+    for (let i = 0; i < length; i++) {
+      switch (bpp) {
+        case 0:
+          colors.push(clp[this._read(4)]);
+          break;
+        case 1:
+          colors.push(clp[this._read(8)]);
+          break;
+        case 2:
+          colors.push({
+            r: Math.round((this._read(5) / 0x1f) * 0xff),
+            g: Math.round((this._read(5) / 0x1f) * 0xff),
+            b: Math.round((this._read(5) / 0x1f) * 0xff),
+            stp: this._read(1),
+          });
+          break;
+        case 3:
+          colors.push({
+            r: this._read(8),
+            g: this._read(8),
+            b: this._read(8),
+          });
+          break;
+      }
     }
-
-    _parse() {
-        this._rewind();
-
-        this._data = {};
-
-        this._data.tag = this._read(8);
-        this._data.version = this._read(8);
-
-        this._skip(16);
-
-        this._data.bpp = this._read(2);
-        this._data.clp = this._read(2);
-
-        this._skip(28);
-
-        if (this._data.clp) {
-            this._data.clutLength = this._read(32);
-            this._data.clutX = this._read(16);
-            this._data.clutY = this._read(16);
-            this._data.clutWidth = this._read(16);
-            this._data.clutHeight = this._read(16);
-            this._data.clutColors = this._readColors(this._data.clutWidth * this._data.clutHeight, 2);
-        }
-
-        this._data.imageLength = this._read(32);
-        this._data.imageX = this._read(16);
-        this._data.imageY = this._read(16);
-        this._data.imageWidth = this._read(16);
-        this._data.imageHeight = this._read(16);
-        switch (this._data.bpp) {
-            case 0:
-                this._data.imageWidth *= 4;
-                break;
-            case 1:
-                this._data.imageWidth *= 2;
-                break;
-            case 3:
-                this._data.imageWidth /= 1.5;
-                break;
-        }
-
-        this._data.imageColors = this._readColors(this._data.imageWidth * this._data.imageWidth, this._data.bpp);
-    }
-
-    _readColors(length, bpp) {
-        const colors = [];
-        for (let i = 0; i < length; i++) {
-            switch (bpp) {
-                case 0:
-                    colors.push(this._data.clutColors[this._read(4)]);
-                    break;
-                case 1:
-                    colors.push(this._data.clutColors[this._read(8)]);
-                    break;
-                case 2:
-                    colors.push({
-                        r: Math.round(this._read(5) / 0x1F * 0xFF),
-                        g: Math.round(this._read(5) / 0x1F * 0xFF),
-                        b: Math.round(this._read(5) / 0x1F * 0xFF),
-                        stp: this._read(1)
-                    });
-                    break;
-                case 3:
-                    colors.push({
-                        r: this._read(8),
-                        g: this._read(8),
-                        b: this._read(8)
-                    });
-                    break;
-            }
-        }
-        return colors;
-    }
-
-    createCanvas() {
-        const data = this._data;
-
-        const canvas = document.createElement('canvas');
-        canvas.width = data.imageWidth;
-        canvas.height = data.imageHeight;
-
-        const ctx = canvas.getContext('2d');
-        const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-        for (let i = 0; i < data.imageColors.length; i++) {
-            ['r', 'g', 'b'].forEach((key, index) => {
-                imageData.data[index + i * 4] = data.imageColors[i][key];
-            });
-            imageData.data[3 + i * 4] = 255;
-        }
-
-        ctx.putImageData(imageData, 0, 0);
-
-        return canvas;
-    }
-
-    readArrayBuffer(arrayBuffer) {
-        this._uint32Array = new Uint32Array(arrayBuffer);
-        this._parse();
-    }
+    return colors;
+  }
 }
